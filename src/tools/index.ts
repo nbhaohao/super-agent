928c6b38517b97977dacd9ad583e8b193b6f9fb6
nbhaohao <nbhaohao@gmail.com>
2026-04-27 12:07:36 +0800

    feat: add new tools

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -4,6 +4,10 @@ import { checkStatusTool } from './check-status.js';
 import { readFileTool } from './read-file.js';
 import { writeFileTool } from './write-file.js';
 import { listDirectoryTool } from './list-directory.js';
+import { editFileTool } from './edit-file.js';
+import { globTool } from './glob.js';
+import { grepTool } from './grep.js';
+import { bashTool } from './bash.js';
 
 export type { ToolDefinition } from './registry.js';
 export { ToolRegistry } from './registry.js';
@@ -11,6 +15,7 @@ export { ToolRegistry } from './registry.js';
 // 主流程工具
 export const allTools = [
     weatherTool, calculatorTool, readFileTool, writeFileTool, listDirectoryTool,
+    editFileTool, globTool, grepTool, bashTool,
 ];
 
 // 循环检测 demo 专用
